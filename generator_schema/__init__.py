6f0f62ef386d9ccafc67bc03feb6432112eb777b
Smrbruh <ohhhgoode@gmail.com>
2026-06-10 16:39:08 +0500

    Use relative import for builder module

--- generator_schema/__init__.py
+++ generator_schema/__init__.py
@@ -1,5 +1,5 @@
 """JSON Schema Draft-07 builder for 1C configuration objects."""
 
-from parser_1c.generator_schema.builder import JsonSchema, SchemaBuilder
+from .builder import JsonSchema, SchemaBuilder
 
 __all__ = ["SchemaBuilder", "JsonSchema"]
